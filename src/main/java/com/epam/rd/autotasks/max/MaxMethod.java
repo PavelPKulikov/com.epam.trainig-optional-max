28d380b32663d9b26f0da81bc53e73523de50ca4
Pavel Kulikov <ppkulikov@gmail.com>
2022-07-18 23:35:49 +0300

    Solved.

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -1,9 +1,16 @@
 package com.epam.rd.autotasks.max;
 
+
 import java.util.OptionalInt;
 
 public class MaxMethod {
     public static OptionalInt max(int[] values) {
-        throw new UnsupportedOperationException();
+        if (values != null && values.length > 0) {
+            int max = values[0];
+            for (int i = 1; i < values.length; i++) {
+                if (values[i] > max) max = values[i];
+            }
+            return OptionalInt.of(max);
+        } else return OptionalInt.empty();
     }
 }
